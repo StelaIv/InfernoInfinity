91ee207a700d225785b0b46a962eeedf998b01b4
StelaIv <ivanova.stela@abv.bg>
2016-07-27 18:15:03 +0300

    Update IFactory.java

--- src/com/company/interfaces/IFactory.java
+++ src/com/company/interfaces/IFactory.java
@@ -1,15 +1,14 @@
 package com.company.interfaces;
 
-import com.company.models.Weapon;
 import com.company.enums.GemType;
 import com.company.enums.WeaponType;
 
 public interface IFactory {
-    Weapon createWeapon(WeaponType weaponType, String weaponName);
+    IWeapon createWeapon(WeaponType weaponType, String weaponName);
 
-    void addGemsToWeapon(Weapon weapon, int socketIndex, GemType gemType);
+    void addGemsToWeapon(IWeapon weapon, int socketIndex, GemType gemType);
 
-    void removeGemFromAWeapon(Weapon weapon, int socketIndex);
+    void removeGemFromAWeapon(IWeapon weapon, int socketIndex);
 
-    String printWeapon(Weapon weapon);
+    String printWeapon(IWeapon weapon);
 }
